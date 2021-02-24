a874b8175b31a956c39be97294da2545591f2125
tanay-pingalkar <tpingalkar@gmail.com>
2021-02-24 15:31:11 +0530

    forget to chalk

--- interpreter/store/utils/throw.ts
+++ interpreter/store/utils/throw.ts
@@ -2,7 +2,7 @@ import chalk = require("chalk");
 import { exit } from "process";
 
 export const throwEr=(error:string,lineNumber:number)=>{
-    console.error(error);
+    console.error(chalk.red(error));
     console.error(chalk.bgRed(`the error has occure on line ${lineNumber+1}`));
     exit();
 }
